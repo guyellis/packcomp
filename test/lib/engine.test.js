b79c1f103c8cc980b12c53bb645a2af25e289cf0
Guy Ellis <wildfiction@gmail.com>
2018-06-20 22:07:47 -0700

    Increase code coverage for tests

--- test/lib/engine.test.js
+++ test/lib/engine.test.js
@@ -1,15 +1,41 @@
-const { getContents } = require('../../lib/engine');
+const {
+  getContents,
+  getTitles,
+  getFilter,
+} = require('../../lib/engine');
 
 describe('cli', () => {
   test('should get contents', () => {
     const argv = {
       _: [
         'test/fixtures/1/',
-        'test/fixtures/2/',
+        'test/fixtures/2/package.json',
         'test/fixtures/3/',
       ],
     };
     const contents = getContents(argv);
     expect(contents).toMatchSnapshot();
+  });
+
+  test('should get titles', () => {
+    const argv = {
+      _: [
+        'test/fixtures/1/',
+        'test/fixtures/2/package.json',
+        'test/fixtures/3/',
+      ],
+    };
+    const contents = getTitles(argv);
+    expect(contents).toMatchSnapshot();
+  });
+
+  test('should get * for no filter', () => {
+    const filter = getFilter({});
+    expect(filter).toBe('*');
+  });
+
+  test('should get string for filter', () => {
+    const filter = getFilter({ f: 'xyz' });
+    expect(filter).toBe('xyz');
   });
 });
